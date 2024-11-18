3d9116ed453cc7f886bc38fffc47962a2a0f2cfa
marjunel09lagman <marjunel09lagman@gmail.com>
2024-11-18 20:32:29 +0800

    added files

---
+++ controllers/project.js
@@ -0,0 +1,36 @@
+import Project from '../models/Project.js';
+
+export const addProject = async (req, res) => {
+    const { title, description, technologies, liveDemo, githubRepo } = req.body;
+    const image = req.file ? `/uploads/${req.file.filename}` : null;
+
+    try {
+      if (!title || !description || !technologies) {
+        return res.status(400).json({ error: 'Title, description, and technologies are required' });
+      }
+  
+      const newProject = new Project({
+        title,
+        description,
+        technologies,
+        liveDemo,
+        githubRepo,
+        image
+      });
+  
+      await newProject.save();
+  
+      res.status(201).json(newProject);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to add project' });
+    }
+  };
+
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch projects' });
+  }
+};
